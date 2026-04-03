e34dee9a75cc96cc09d7682b67c8513d62c4fa4b
Eric Martinez <ericmartinez1007@gmail.com>
2026-04-03 14:42:48 -0500

    Finished NewTeam.jsx code for creating a new team obj in the database then directing the user to edit team page

--- src/components/newTeam/NewTeam.jsx
+++ src/components/newTeam/NewTeam.jsx
@@ -1,5 +1,56 @@
-export const NewTeam = () => {
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { createTeam } from "../services/teamServices"
+
+export const NewTeam = ({ currentUser }) => {
+    const [newTeam, setNewTeam] = useState({
+        name: "",
+        userId: currentUser.id,
+        formatId: 1
+    })
+
+    const navigate = useNavigate()
+
+    const updateNewTeam = (evt) => {
+        const copy = { ...newTeam }
+        copy[evt.target.id] = evt.target.value
+        setNewTeam(copy)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        createTeam(newTeam).then((createdTeam) => {
+            navigate(`/editteam/${createdTeam.id}`)
+        })
+    }
+    
+    
     return (
-        <>Welcome to the New Team Page</>
+        <main className="page-container">
+            <section>
+                <h1 className="page-title">Create a New Team</h1>
+                <span className="page-subtitle">Build a new competitive team!</span>
+                <form className="form-newteam" onSubmit={handleSubmit}>
+                <h2>Team Name</h2>
+                <fieldset>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            id="name"
+                            value={newTeam.name}
+                            onChange={updateNewTeam}
+                            className="form-teamname"
+                            placeholder="Enter a team name"
+                            required
+                            autoFocus
+                        />
+                    </div>
+                </fieldset>
+                <button type="submit" className="btn-create-team">
+                    Create Team
+                </button>
+                </form>
+            </section>
+        </main>
     )
 }
---
+++ src/components/services/teamServices.js
@@ -0,0 +1,19 @@
+export const createTeam = (newTeam) => {
+  return fetch("http://localhost:8088/teams", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newTeam),
+  }).then((res) => res.json())
+}
+
+//Finds and returns the obj that has a matching teamId to the current teamId in the URL params 
+export const getTeamById = (teamId) => {
+    return fetch(`http://localhost:8088/teams/${teamId}`).then((res) => res.json())
+}
+
+//Need to filter through all objs in pokemonTeams since we can have multiple hits matching the current teamId in the URL params
+export const getPokemonByTeamId = (teamId) => {
+    return fetch(`http://localhost:8088/pokemonTeams?teamId=${teamId}&_expand=pokemon`).then((res) => res.json())
+}
